07939045e134836533caf43f92e17135f2c0463f
AnnaOW <anna.wierzbicka1996@gmail.com>
2023-12-13 20:32:35 +0100

    Restructured components, added formated date & weather information

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import "./styles.css";
-import Search from "./Search";
-import "./Search.css";
-import Summary from "./Summary";
-import "./Summary.css";
+import Weather from "./Weather";
+import "./Weather.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="weather-app container">
-        <Search />
-        <h1>
-          <strong>Verona</strong>
-        </h1>
-        <h4>Friday, 24 November 2023, 20:11</h4>
-
-        <img
-          className="weather-picture"
-          src="https://media.istockphoto.com/id/1368722560/it/vettoriale/disegno-cute-sun-cartoon-illustrazione-vettoriale-su-sfondo-bianco-illustrazione.jpg?s=1024x1024&w=is&k=20&c=sZ5985JBfghfSjY9T_htzk2dR1YVtQ78plok8nzW3vU="
-          alt="Sun"
-        />
-
-        <Summary />
+        <Weather defaultCity="Rome" />
       </div>
       <footer>
         This project was coded by Anna Oliwia Wierzbicka, and is{" "}
-        <a href="https://github.com/AnnaOW/weather-app-react">open-sourced</a>.
+        <a
+          href="https://github.com/AnnaOW/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+        .
       </footer>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+import axios from "axios";
+import "./Weather.css";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coordinates,
+      temperature: Math.round(response.data.temperature.current),
+      humidity: response.data.temperature.humidity,
+      date: new Date(response.data.time * 1000),
+      description: response.data.condition.description,
+      icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+      wind: Math.round(response.data.wind.speed),
+      city: response.data.city,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "9a4cbff04f4e654ca4teaa03bc88aoaf";
+    let units = "metric";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="find-your-city-search w-50 p-3"
+            type="text"
+            name="city"
+            required
+            placeholder="Type your city here"
+            autoComplete="off"
+            autoFocus="on"
+            onChange={handleCityChange}
+          />
+          <input
+            type="submit"
+            className="btn btn-primary btn-lg search-city-button"
+            data-bs-toggle="button"
+            autoComplete="off"
+            value="Search"
+          />
+        </form>
+
+        {<WeatherInfo data={weatherData} />}
+        {/* <WeatherForecast coordinates={weatherData.coordinates} /> */}
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
+}
