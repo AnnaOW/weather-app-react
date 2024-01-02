8de0366900ad6a8ebf7336d867c0e7c74e109981
AnnaOW <anna.wierzbicka1996@gmail.com>
2024-01-02 20:59:48 +0100

    Built 5-day weather forecast

---
+++ src/WeatherForecast.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import WeatherForecastDay from "./WeatherForecastDay";
+import "./WeatherForecast.css";
+import axios from "axios";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  function load() {
+    let apiKey = "9a4cbff04f4e654ca4teaa03bc88aoaf";
+    let units = "metric";
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return <div className="col" key={index}></div>;
+              <WeatherForecastDay data={dailyForecast} />;
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
+
+    return null;
+  }
+}
